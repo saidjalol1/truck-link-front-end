106abcfa305b9d9a9b090c033e7833c3c99bb1b9
Saidjalol Turakhujayev <turakhujayevsaidjalol06@gmail.com>
2025-05-27 20:11:42 +0500

    menu bugs fixed

--- front_end/components/truckers/SidebarMenu.vue
+++ front_end/components/truckers/SidebarMenu.vue
@@ -1,14 +1,17 @@
 <script setup>
 
-
+const activeIndex = ref(0)
 </script>
 <template>
     <div class="menu shadow p-3">
         <div class="logotip">
             <NuxtImg src="images/ChatGPT Image May 17, 2025, 06_06_51 PM.png"/>
         </div>
-        <div class="menus mt-6 flex flex-col gap-6">
-            <div class="item flex flex-col items-center cursor-pointer hover:text-blue-600">
+        <div class="menus mt-6 flex flex-col">
+            <div class="item px-2 py-2 rounded-lg flex flex-col items-center cursor-pointer hover:text-blue-600"
+                :class="{'bg-gray-100 text-blue-600': activeIndex === 0}"
+                @click="activeIndex = 0">
+
                 <div class="logo  p-2 rounded-lg">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list-task" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5zM3 3H2v1h1z"/>
@@ -16,9 +19,11 @@
                         <path fill-rule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5zM2 7h1v1H2zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm1 .5H2v1h1z"/>
                     </svg>
                 </div>
-                <div class="text text-xs mt-1 hover:text-blue-400">Jo'natmalar</div>
+                <div class="text text-xs mt-1 hover:text-blue-400">Jo'natma</div>
             </div>
-            <div class="item flex flex-col items-center cursor-pointer hover:text-blue-600">
+            <div class="item px-2 py-2  rounded-lg flex flex-col items-center cursor-pointer hover:text-blue-600"
+                :class="{'bg-gray-100 text-blue-600': activeIndex === 1}" @click="activeIndex = 1">
+
                 <div class="logo  p-2 rounded-lg">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-clock-history" viewBox="0 0 16 16">
                         <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022zm2.004.45a7 7 0 0 0-.985-.299l.219-.976q.576.129 1.126.342zm1.37.71a7 7 0 0 0-.439-.27l.493-.87a8 8 0 0 1 .979.654l-.615.789a7 7 0 0 0-.418-.302zm1.834 1.79a7 7 0 0 0-.653-.796l.724-.69q.406.429.747.91zm.744 1.352a7 7 0 0 0-.214-.468l.893-.45a8 8 0 0 1 .45 1.088l-.95.313a7 7 0 0 0-.179-.483m.53 2.507a7 7 0 0 0-.1-1.025l.985-.17q.1.58.116 1.17zm-.131 1.538q.05-.254.081-.51l.993.123a8 8 0 0 1-.23 1.155l-.964-.267q.069-.247.12-.501m-.952 2.379q.276-.436.486-.908l.914.405q-.24.54-.555 1.038zm-.964 1.205q.183-.183.35-.378l.758.653a8 8 0 0 1-.401.432z"/>
@@ -28,7 +33,8 @@
                 </div>
                 <div class="text text-xs mt-1 hover:text-blue-400">Tarix</div>
             </div>
-            <div class="item flex flex-col items-center cursor-pointer hover:text-blue-600">
+            <div class="item px-2 py-2  rounded-lg flex flex-col items-center cursor-pointer hover:text-blue-600"
+                :class="{'bg-gray-100 text-blue-600': activeIndex === 2}" @click="activeIndex = 2">
                 <div class="logo  p-2 rounded-lg">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
                         <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
@@ -37,7 +43,8 @@
                 </div>
                 <div class="text text-xs mt-1 hover:text-blue-400">Xarita</div>
             </div>
-            <div class="item flex flex-col items-center cursor-pointer hover:text-blue-600">
+            <div class="item px-2 py-2 rounded-lg flex flex-col items-center cursor-pointer hover:text-blue-600"
+                :class="{'bg-gray-100 text-blue-600': activeIndex === 3}" @click="activeIndex = 3">
                 <div class="logo  p-2 rounded-lg">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
                         <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
--- front_end/components/truckers/header.vue
+++ front_end/components/truckers/header.vue
@@ -31,40 +31,40 @@ function getLimitedFullName(name, surname, maxLength = 10) {
     </div>
     <div v-if="profile" class="profile-drop shadow rounded text-gray-300">
         <div class="block-1 flex items-center gap-4 p-4 hover:bg-gray-100 cursor-pointer">
-            <div class="text-gray-400 font-bold">
+            <div class="text-gray-400 font-bold text-xs">
                 saidjalolturakhujayev@gmail.com
             </div>
         </div>
         <hr>
-        <div class="block-1 flex items-center gap-4 p-4 hover:bg-gray-100 cursor-pointer">
+        <div class="block-1 flex items-center gap-4 p-2 px-4 hover:bg-gray-100 cursor-pointer">
             <div class="cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-pencil" viewBox="0 0 16 16">
                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                 </svg>
             </div>
-            <div class="text-black font-bold text-sm">
+            <div class="text-black font-bold text-xs">
                 Tahrirlash
             </div>
         </div>
-        <div class="block-1 flex items-center gap-4 p-4 hover:bg-gray-100 cursor-pointer">
+        <div class="block-1 flex items-center gap-4 p-2 px-4 hover:bg-gray-100 cursor-pointer">
             <div class="cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
                     <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
                 </svg>
             </div>
-            <div class="text-black font-bold text-sm">
+            <div class="text-black font-bold text-xs">
                 Chiqish
             </div>
         </div>
-        <div class="block-1 flex items-center gap-4 p-4 hover:bg-gray-100 cursor-pointer">
+        <div class="block-1 flex items-center gap-4 p-2 px-4 hover:bg-gray-100 cursor-pointer">
             <div class="cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-info-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
                 </svg>
             </div>
-            <div class="text-black font-bold text-sm">
+            <div class="text-black font-bold text-xs">
                 Yordam
             </div>
         </div>
